cdc1affeebefd69df72343529adcd5e43459b49a
mahipanc <panchalmahesh727@gmail.com>
2023-10-03 13:54:54 +0530

    Added new mthod

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -33,5 +33,21 @@ namespace WebApplication1.Controllers
             })
             .ToArray();
         }
+
+        /// <summary>
+        /// Added new method
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet(Name = "GetWeatherForecast1")]
+        public IEnumerable<WeatherForecast> Get1()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
